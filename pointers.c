5a99871b6056b8647ddbb603a345e2261828a32a
Andrew <andrew@mslab21.asfa.local>
2016-11-04 11:27:12 -0400

    Program to start using pointers

---
+++ pointers.c
@@ -0,0 +1,55 @@
+/*
+	pointers.c
+	Andrew Hess
+	11/2/2016
+	The user enters a number. Create a pointer for that number and 
+	allow the user to find the pointer value, pointer address, and
+	integer value
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int num = 0;
+	int * ptr;
+	ptr = &num;
+
+	int done = 0;
+	while (!done) {
+		printf("Enter a integer: ");
+		scanf("%d", &num);
+		getchar();
+		
+		printf("Enter 1 to get the pointer value\n");
+		printf("Enter 2 to get the pointer address\n");
+		printf("Enter 3 to get the integer value\n");
+	
+		char input = getchar();
+		while (getchar() != '\n');
+
+		printf("input: '%c'\n", input);
+
+		switch (input) {
+			case '1':
+				printf("Pointer value: %d\n", ptr);
+				break;
+			case '2':
+				printf("Pointer address: %p\n", ptr);
+				break;
+			case '3':
+				printf("Integer value: %d\n", num);
+				break;
+			default:
+				printf("Invalid option\n");
+		}
+
+		printf("Continue? (y/n): ");
+		input = getchar();
+		while (getchar() != '\n');
+
+		if (input == 'n' || input == 'N') done = 1;
+	}
+
+	exit(0);
+}
