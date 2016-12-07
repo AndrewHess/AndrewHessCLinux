ebfdcf23c34b1613956bb932e0a2429e7dd6b02b
Luke Hess <lukehess@Jennifer-Hesss-MacBook-Pro-2.local>
2016-12-07 07:03:25 -0600

    Updated the dump function

--- simpletron.c
+++ simpletron.c
@@ -30,8 +30,6 @@ int main() {
 	printf("*** Type the sentinel -99999 to stop entering ***\n");
 	printf("*** your program. ***\n");
 
-	dump(acc, instCount, instReg, opCode, operand, mem);
-
 	// read in the program from the user
 	int inc = 0;
 	int * wordptr = malloc(sizeof(wordptr));
@@ -41,11 +39,11 @@ int main() {
 		scanf("%d", wordptr);
 	
 		if ((*wordptr < -9999 || *wordptr > 9999) && *wordptr != -99999) {
-			pritnf("*** Invalid input ***\n");
+			printf("*** Invalid input ***\n");
 			inc--;
 		}
-		else {
-			mem[ind -1] = *wordptr;
+		else if (*wordptr != -99999) {
+			mem[inc -1] = *wordptr;
 		}
 	}
 
@@ -55,12 +53,12 @@ int main() {
 }
 
 void dump (int acc, int instCount, int instReg, int opCode, int operand, int * mem) {
-	printf("REGISTERS:\n");
-	printf("accumulator		%04d\n", acc);
-	printf("instructionCounter	%02d\n", instCount);
-	printf("instructionRegister	%04d\n", instReg);
-	printf("operationCode		%02d\n", opCode);
-	pritnf("operand			%02d\n", operand);
+	printf("\nREGISTERS:\n");
+	printf("accumulator		%+05d\n", acc);
+	printf("instructionCounter	%+03d\n", instCount);
+	printf("instructionRegister	%+05d\n", instReg);
+	printf("operationCode		%+03d\n", opCode);
+	printf("operand			%+03d\n", operand);
 
 	printf("\nMEMORY:\n\t");
 	for (int i = 0; i < 10; i++) {
@@ -70,10 +68,11 @@ void dump (int acc, int instCount, int instReg, int opCode, int operand, int * m
 	printf("\n");
 	for (int i = 0; i < 10; i++) {
 		printf("%d\t", 10*i);
-		for (int k = 0; i < 10; k++) {
-			printf("%+04d\t", mem[10*i + k]);
+		for (int k = 0; k < 10; k++) {
+			printf("%+05d\t", mem[10*i + k]);
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
 		 
