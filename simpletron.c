02699195029fc67dbe1fb2ef435168133548d2bd
Andrew <andrew@mslab21.asfa.local>
2016-12-07 14:41:59 -0500

    Added switch statement

--- simpletron.c
+++ simpletron.c
@@ -8,7 +8,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define READ 10
+#define WRITE 11
+#define LOAD 20
+#define STORE 21
+#define ADD 30
+#define SUBTRACT 31
+#define DIVIDE 32
+#define MULTIPLY 33
+#define BRANCH 40
+#define BRANCHNEG 41
+#define BRANCHZERO 42
+#define HALT 43
+
 void dump (int acc, int instCount, int instReg, int opCode, int operand, int * mem);
+void error (int acc, int instCount, int instReg, int opCode, int operand, int * mem);
 
 int main() {
 	// create the accumulator and memory
@@ -47,6 +61,72 @@ int main() {
 		}
 	}
 
+	// start running the sml program
+	int running = 1;
+	while (running) {
+		// put the next instruction into the register
+		instReg = mem[instCount];
+
+		// get opCode and operand
+		opCode  = instReg / 100;
+		operand = instReg % 100;
+
+		// do the correct command
+		switch (opCode) {
+			case READ:
+				printf("Enter a number: ");
+				scanf("%d", mem + sizeof(int)*operand);
+				printf("Done reading\n");
+				break;
+			case WRITE:
+				printf("%d\n", mem[operand]);
+				break;
+			case LOAD:
+				acc = mem[operand];
+				break;
+			case STORE:
+				mem[operand] = acc;
+			case ADD:
+				acc += mem[operand];
+			case SUBTRACT:
+				acc -= mem[operand];
+			case DIVIDE:
+				if (mem[operand] == 0) {
+					printf("*** Divide by zero ***\n");
+					error(acc, instCount, instReg, opCode, operand, mem);
+				}
+				else {
+					acc /= mem[operand];
+				}
+				break;	
+			case MULTIPLY:
+				acc *= mem[operand];
+				break;
+			case BRANCH:
+				instCount = operand -1;
+				break;
+			case BRANCHNEG:
+				if (acc < 0) {
+					instCount = operand -1;
+				}
+				break;
+			case BRANCHZERO:
+				if (acc == 0) {
+					instCount = operand -1;
+				}
+				break;
+			case HALT:
+				running = 0;
+				break;
+			default:
+				printf("*** Unknown command: %02d ***\n", opCode);
+				running = 0;
+				error(acc, instCount, instReg, opCode, operand, mem);
+		}
+
+		instCount++;
+	}
+
 	dump(acc, instCount, instReg, opCode, operand, mem);
 
 	exit(0);
@@ -55,10 +135,10 @@ int main() {
 void dump (int acc, int instCount, int instReg, int opCode, int operand, int * mem) {
 	printf("\nREGISTERS:\n");
 	printf("accumulator		%+05d\n", acc);
-	printf("instructionCounter	%+03d\n", instCount);
+	printf("instructionCounter	%02d\n", instCount);
 	printf("instructionRegister	%+05d\n", instReg);
-	printf("operationCode		%+03d\n", opCode);
-	printf("operand			%+03d\n", operand);
+	printf("operationCode		%02d\n", opCode);
+	printf("operand			%02d\n", operand);
 
 	printf("\nMEMORY:\n\t");
 	for (int i = 0; i < 10; i++) {
@@ -74,5 +154,9 @@ void dump (int acc, int instCount, int instReg, int opCode, int operand, int * m
 		printf("\n");
 	}
 	printf("\n");
+} 
+
+void error (int acc, int instCount, int instReg, int opCode, int operand, int * mem) {
+	printf("*** Simpletron execution abnormally terminated ***\n");
+	dump(acc, instCount, instReg, opCode, operand, mem);
 }
-		 
